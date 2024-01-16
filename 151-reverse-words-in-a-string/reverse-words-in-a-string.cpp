a71b29ef59a9bc51359abe9d0f9bd11f5caf37ec
RI3C <42501008+RI3C@users.noreply.github.com>
2024-01-16 20:41:31 +0530

    Time: 7 ms (44.86%) | Memory: 21.4 MB (12.61%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -6,20 +6,18 @@ public:
         int n=s.size();
         string word="";
         int space=0;
+        string ans="";
         for(int i=0;i<n;i++){
             if(s[i]!=' ') word+=s[i],space=0;
             else {
                 if(s[i]==' ' && !space && word!="") {
-                    words.push_back(word);
+                    ans=" "+word+ans;
                     word="";
                 }
                 space=1;
             }
         }
-        reverse(words.begin(),words.end());
-        string ans="";
-        for(string st: words) ans+=st+" ";
-        ans.erase(ans.size()-1);
+        ans.erase(ans.begin());
         return ans;
     }
 };
