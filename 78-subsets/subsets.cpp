dee98af2dd44edef80132dd6ada5a8ce8539024c
RI3C <42501008+RI3C@users.noreply.github.com>
2024-03-03 00:37:55 +0530

    Time: 4 ms (40.45%) | Memory: 8.2 MB (72.29%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    void powerSet(int index, vector<int> &currSet, int n, vector<int>& nums, vector<vector<int>> &ans){
-        if(index>=n) {
-            ans.push_back(currSet);
-            return;
-        }
-        
-        currSet.push_back(nums[index]);
-        powerSet(index+1,currSet,n,nums,ans);
-        currSet.pop_back();
-        powerSet(index+1,currSet,n,nums,ans);
-    }
-
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> currSet;
         int n=nums.size();
-        powerSet(0,currSet,n,nums,ans);
+        for(int i=0;i<1<<n;i++){
+            vector<int> currSet;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)) currSet.push_back(nums[j]);
+            }
+            ans.push_back(currSet);
+        }
         return ans;
     }
 };
