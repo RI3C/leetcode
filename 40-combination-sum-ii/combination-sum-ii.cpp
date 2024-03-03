f9df617402633ce9e77dbfc8c96d0e07d8f71593
RI3C <42501008+RI3C@users.noreply.github.com>
2024-03-03 18:51:27 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (64.24%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void sumCombo(int index, int target, vector<int> &currSet, vector<int> &candidates, vector<vector<int>> &ans){
+        if(target==0) {
+            ans.push_back(currSet);
+            return;
+        }
+        
+        for(int idx=index; idx<candidates.size(); idx++){
+            if(candidates[idx]>target) break;
+            if(idx>index && candidates[idx]==candidates[idx-1]) continue;
+            currSet.push_back(candidates[idx]);
+            sumCombo(idx+1,target-candidates[idx],currSet,candidates,ans);
+            currSet.pop_back();
+        }
+    }
+
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>> ans;
+        vector<int> currSet;
+        sumCombo(0,target,currSet,candidates,ans);
+        return ans;    
+    }
+};
