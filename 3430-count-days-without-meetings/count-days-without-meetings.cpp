3dea3272c008a5207a5eb987c9664788b6821ca7
RI3C <42501008+RI3C@users.noreply.github.com>
2024-06-03 12:03:49 +0530

    Time: 369 ms (16.67%) | Memory: 150 MB (16.67%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& meetings) {
+        int n=meetings.size();
+        vector<pair<int,int>> initialMeetings;
+        for(auto i: meetings){
+            initialMeetings.push_back({i[0],i[1]});
+        }
+        sort(initialMeetings.begin(),initialMeetings.end());
+        // for(auto i: initialMeetings){
+        //     cout<<i.first<<" "<<i.second<<"\n";
+        // }
+        int prevEnd=-1;
+        int prevStart=-1;
+        vector<pair<int,int>> mergedMeetings;
+        for(auto i: initialMeetings){
+            if(i.first>prevEnd){
+                mergedMeetings.push_back(i);
+                prevEnd=i.second;
+                prevStart=i.first;
+            }
+            else{
+                int newStart=min(prevStart,i.first);
+                int newEnd=max(prevEnd,i.second);
+                mergedMeetings.pop_back();
+                mergedMeetings.push_back({newStart,newEnd});
+                prevEnd=newEnd;
+                prevStart=newStart;
+            }
+        }
+        int ans=0,totalMeetDays=0;
+        for(auto i: mergedMeetings){
+            totalMeetDays+=(i.second-i.first+1);
+        }
+        // for(auto i: mergedMeetings){
+        //     cout<<i.first<<" "<<i.second<<"\n";
+        // }
+        ans=days-totalMeetDays;
+        return ans;
+    }
+};
